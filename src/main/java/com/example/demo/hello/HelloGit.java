65357311d44d1feeb933ef7ad2329a4d5fe754a0
likun0404 <1194104909@qq.com>
2021-08-03 16:13:21 +0800

    First commit all

--- src/main/java/com/example/demo/hello/HelloGit.java
+++ src/main/java/com/example/demo/hello/HelloGit.java
@@ -2,6 +2,6 @@ package com.example.demo.hello;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git , I comt");
+        System.out.println("Hello Git , I ");
     }
 }
